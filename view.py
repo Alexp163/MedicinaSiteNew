7d5b78e4b3057e2cca14ae62696127cdaf2f91a9
Alexp163 <dr.p163@ya.ru>
2023-06-27 22:30:44 +0400

    Добавили ссылку на личный кабинет.

--- view.py
+++ view.py
@@ -7,3 +7,6 @@ def index():
     name = 'Ivan'
     return render_template('single.html', n=name)
 
+@app.route('/select_action')
+def select_action():
+    return render_template('selection_page.html')
