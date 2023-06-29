fb9c2f3420fb66d2293b3205f3602663a3fc1d8e
Alexp163 <dr.p163@ya.ru>
2023-06-29 23:27:04 +0400

    Добавили ссылку на регистрацию и авторизацию в личном кабинете

--- view.py
+++ view.py
@@ -10,3 +10,28 @@ def index():
 @app.route('/select_action')
 def select_action():
     return render_template('selection_page.html')
+
+@app.route('/admin_select')
+def admin_select():
+    return render_template('admin_select.html')
+
+@app.route('/autorization_user')
+def autorization_user():
+    return render_template('authorization.html')
+
+@app.route('/registration_user')
+def registration_user():
+    return render_template('registration.html')
+
+@app.route('/patient_data')
+def patient_data():
+    return render_template('patient_data.html')
+
+@app.route('/selection_page')
+def selection_page():
+    return ('selection_page.html')
+
+@app.route('/karloson_consultation')
+def doctor_consultation():
+    return ('karloson.html')
+
